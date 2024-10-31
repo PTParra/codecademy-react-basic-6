75eddce38cb10f34ae2dec92d2fc199150c31622
PTParra <49121638+PTParra@users.noreply.github.com>
2024-10-31 09:43:06 +0100

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Game } from './components/game';
+
+/*
+  Styling Rock, Paper, Scissors
+
+  In this project, you will get the chance to practice styling React applications using different techniques.
+
+  You will be styling a game of Rock, Paper, Scissors. However, rather than sticking to one technique, you will 
+  be asked to modify its appearance using inline styling syntax, object variable syntax, and, finally, CSS modules!
+  
+*/
+
+export const App = () => {
+
+	return (
+      <Game />
+    );
+}
